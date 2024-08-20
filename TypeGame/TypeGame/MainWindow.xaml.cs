8dcc84b8973924d467dc4ed935fc9320253ab33b
Cynastic <111385987+Cynastic@users.noreply.github.com>
2024-08-21 00:01:24 +0200

    Init

---
+++ TypeGame/TypeGame/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Animation;
+
+namespace TypeGame
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			TextManager.Initialize();
+			ChangeText(TextManager.currentText);
+			WriteTextBox.Focus();
+			Dispatcher.BeginInvoke(AlignTextBoxes, System.Windows.Threading.DispatcherPriority.ContextIdle);
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			
+			for(int i = 0; i < WriteTextBox.Text.Length; i++)
+			{
+				if (TextManager.currentText[i] != WriteTextBox.Text[i])
+				{
+					WriteTextBox.Text = WriteTextBox.Text.Remove(i, WriteTextBox.Text.Length - i);
+					WriteTextBox.CaretIndex = i;
+					WrongCharTyped();
+				}
+			}
+			//check Win Condition
+			if(WriteTextBox.Text == TextManager.currentText)
+			{
+				TextTyped();
+			}
+		}
+
+		void TextTyped()
+		{
+			WriteTextBox.Text = "";
+			TextManager.TextWritten();
+			ChangeText(TextManager.currentText);
+			Dispatcher.BeginInvoke(AlignTextBoxes, System.Windows.Threading.DispatcherPriority.ContextIdle);
+		}
+
+		void AlignTextBoxes()
+		{
+			WriteTextBox.MinWidth = ShowTextBox.ActualWidth;
+			WriteTextBox.MinHeight = ShowTextBox.ActualHeight;
+			WriteTextBox.Width = ShowTextBox.ActualWidth;
+			WriteTextBox.Height = ShowTextBox.ActualHeight;
+		}
+
+		void WrongCharTyped()
+		{
+			ColorAnimation colorFade = new ColorAnimation(Colors.Red, Color.FromRgb(66, 66, 66), new Duration(TimeSpan.FromMilliseconds(400)));
+			Storyboard.SetTarget(colorFade, ShowTextBox);
+			Storyboard.SetTargetProperty(colorFade, new PropertyPath("Foreground.Color"));
+
+			Storyboard sb = new();
+			sb.Children.Add(colorFade);
+			sb.Begin();
+		}
+
+		public void ChangeText(string text)
+		{
+			ShowTextBox.Text = text;
+		}
+
+		private void Window_ContentRendered(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ TypeGame/TypeGame/TextMan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TypeGame
+{
+	static class TextManager 
+	{
+		static Random random = new();
+		public static string currentText = "";
+
+		static List<string> textList = new();
+
+		public static void Initialize()
+		{
+			textList = File.ReadAllLines("quotes.txt").ToList();
+			currentText = GetRandomText();
+		}
+
+		public static void TextWritten()
+		{
+			currentText = GetRandomText();
+		}
+		
+		private static string GetRandomText()
+		{
+			int randIndex = random.Next(0, textList.Count);
+			while (textList[randIndex] == currentText)
+			{
+				randIndex = random.Next(0, textList.Count);
+			}
+			return textList[randIndex];
+		}
+	}
+}
